fafabb5ba6f51c876964753066f7fdbad7359a96
James Hsu <jameshsu1125@gmail.com>
2025-07-13 14:17:52 +0800

    Refactor code structure for improved readability and maintainability

--- src/configs/constants/layout.constants.ts
+++ src/configs/constants/layout.constants.ts
@@ -1,26 +1,3 @@
-export const TILES_CONTAINER_SCALE_RATIO = 0.6;
-export const ENEMY_AVATAR = {
-  CONTAINER: {
-    OFFSET_X: -0.35,
-  },
-  IMAGE: {
-    WIDTH_SCALE_RATIO: 0.2,
-    OFFSET_Y: 0.125,
-  },
-};
-export const REMAINING_MOVES = {
-  OFFSET_X: 0.35,
-  MOVES_TEXT: {
-    OFFSET_X: 0,
-    OFFSET_Y: -15,
-  },
-  MOVES_COUNT: {
-    OFFSET_X: 0,
-    OFFSET_Y: 10,
-  },
-};
-export const MATCH_FIGHT_LOGO_WIDTH_SCALE_RATIO = 0.35;
-export const TOP_BAR_OFFSET_Y = -0.4;
 export const VICTORY_OFFSET_Y = -0.1;
 
 export const LOGO_WIDTH_SCALE_RATIO = 0.45625;
